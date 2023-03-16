bfc1c92caad3afe759622484aebdbb911db37e0a
nayu <19748682+nayu7u@users.noreply.github.com>
2023-03-17 00:36:54 +0900

    style: go fmt

--- abc293/main.go
+++ abc293/main.go
@@ -8,7 +8,7 @@ func main() {
 	fmt.Scan(&s)
 
 	for i := 0; i < len(s)/2; i++ {
-    fmt.Print(s[2*i+1:2*i+2])
-    fmt.Print(s[2*i:2*i+1])
+		fmt.Print(s[2*i+1 : 2*i+2])
+		fmt.Print(s[2*i : 2*i+1])
 	}
 }
